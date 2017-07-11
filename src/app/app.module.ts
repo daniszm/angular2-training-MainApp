7e8e81cddad7ad7906c62e467356a77dab14aab4
daniszm <daniszek9933@gmail.com>
2017-07-11 09:18:15 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import { ItemsComponent } from './components/items/items.component';
+import { WorkersComponent } from './components/workers/workers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    WorkersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'items', component: ItemsComponent},
+      {path: 'workers', component: WorkersComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
