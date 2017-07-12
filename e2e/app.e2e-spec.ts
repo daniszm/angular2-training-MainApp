9b48880cc8d95cfa75ff022246b455b22f4ae0a7
daniszm <daniszek9933@gmail.com>
2017-07-12 14:52:51 +0200

    Testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,56 @@
-import { NgMainAppKrkPage } from './app.po';
+import {browser, By, element} from "protractor";
+describe('items view', () => {
 
-describe('ng-main-app-krk App', () => {
-  let page: NgMainAppKrkPage;
+  const btnLogIn = element(By.buttonText('log in'));
+  const btnAddItem = element(By.buttonText('add item'));
+  const addItemForm = element(By.css('form.modal-add'));
+
+  beforeAll(() => {
+      browser.get('/');
+      expect(btnLogIn.isPresent()).toBeTruthy();
+      btnLogIn.click();
+      expect(btnLogIn.isPresent()).toBeFalsy();
+  });
 
   beforeEach(() => {
-    page = new NgMainAppKrkPage();
-  });
+      browser.get('/items')
+  })
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
-  });
-});
+  it('should display items', () => {
+      browser.get('/items');
+      browser.sleep(10000);
+  })
+
+  it('should display add item form', () => {
+    btnAddItem.click();
+    expect(addItemForm.isPresent()).toBeTruthy();
+  })
+
+  it('should add new item', () => {
+
+    const uniqueName = "Danisz " + Date.now();
+
+    btnAddItem.click();
+    const titleField = addItemForm.element(By.css('input[name=title]'));
+    const priceField = addItemForm.element(By.css('input[name=price]'));
+    const descField = addItemForm.element(By.css('textarea'));
+    const categorySelect = addItemForm.element(By.css('select'));
+    const categoryField = categorySelect.element(By.cssContainingText('option', 'food')).click();
+    const btnSendForm = addItemForm.element(By.buttonText('send item'));
+
+    const searchByTitle = element(By.css('app-search input[ng-reflect-name=title'))
+
+    titleField.sendKeys(uniqueName);
+    priceField.sendKeys('1234');
+    descField.sendKeys('cccc sdfa');
+    browser.sleep(5000);
+
+    btnSendForm.click();
+
+    searchByTitle.sendKeys(uniqueName);
+
+    browser.sleep(5000);
+
+  })
+
+})
